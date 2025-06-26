602417e74a2636b74fe5bef619ffa406fa6dfdc7
Eeury <callmeeury@gmail.com>
2025-06-26 19:31:49 +0300

    Initial commit

--- payments/urls.py
+++ payments/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api/payment/stkpush/", views.stk_push),
-    path("api/payment/callback/", views.mpesa_callback, name="mpesa_callback"),
-    path("api/payment/status/", views.payment_status, name="payment_status"),
+    path("payment/stkpush/", views.stk_push),
+    path("payment/callback/", views.mpesa_callback, name="mpesa_callback"),
+    path("payment/status/", views.payment_status, name="payment_status"),
 ]
