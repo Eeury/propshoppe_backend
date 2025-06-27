3960395bc46a3a2ce1ed5be1d55fde983bdbb2c3
Eeury <callmeeury@gmail.com>
2025-06-27 06:27:06 +0300

    Initial commit

--- prop_shoppe/wsgi.py
+++ prop_shoppe/wsgi.py
@@ -1,16 +1,16 @@
-
 """
 WSGI config for prop_shoppe project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+# NEW: run migrations automatically on startup
+from django.core.management import call_command
+call_command('migrate')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prop_shoppe.settings')
 
