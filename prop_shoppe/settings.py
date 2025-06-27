f7fa04026bc5db8c8f5cdeef5a7ee66377c971e1
Eeury <callmeeury@gmail.com>
2025-06-27 06:07:47 +0300

    Intial commit

--- prop_shoppe/settings.py
+++ prop_shoppe/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-h*4n2a^*91cm$j%-(gj)&-fxlgjp5$un&2m)l#o)kps^m)!lt2')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['propshoppe-backend.onrender.com', '*']
 print("ALLOWED_HOSTS:", ALLOWED_HOSTS)
 
 INSTALLED_APPS = [
