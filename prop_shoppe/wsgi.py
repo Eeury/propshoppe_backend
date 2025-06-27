1c90ac2f93d187e314a4a6bfe6f9f83de1518451
Eeury <callmeeury@gmail.com>
2025-06-27 06:35:59 +0300

    Intial commit

--- prop_shoppe/wsgi.py
+++ prop_shoppe/wsgi.py
@@ -6,12 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
-# NEW: run migrations automatically on startup
-from django.core.management import call_command
-call_command('migrate')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prop_shoppe.settings')
+
+from django.core.wsgi import get_wsgi_application
+
+
+from django.core.management import call_command
+call_command('migrate')
 
 application = get_wsgi_application()
