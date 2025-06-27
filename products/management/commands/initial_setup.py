92dd149d3821346ff9fd828999cda579bf44b5b0
Eeury <callmeeury@gmail.com>
2025-06-27 07:03:12 +0300

    admin panel

---
+++ products/management/commands/initial_setup.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = "Run all migrations and create a default superuser"
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write(self.style.SUCCESS("Running migrations..."))
+        call_command("migrate")
+
+        User = get_user_model()
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("admin", "callmeeury@gmail.com", "clautas12")
+            self.stdout.write(self.style.SUCCESS("Created superuser: admin / adminpass123"))
+        else:
+            self.stdout.write(self.style.WARNING("Superuser already exists"))
--- prop_shoppe/wsgi.py
+++ prop_shoppe/wsgi.py
@@ -6,13 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 import os
 
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prop_shoppe.settings')
 
 from django.core.wsgi import get_wsgi_application
-
-
-from django.core.management import call_command
-call_command('migrate')
 
 application = get_wsgi_application()
