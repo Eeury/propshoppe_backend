4997267670bc5d2c73e5eec0e8362a6bedff443f
Eeury <callmeeury@gmail.com>
2025-07-01 17:36:46 +0300

    Initial commit

--- prop_shoppe/settings.py
+++ prop_shoppe/settings.py
@@ -89,7 +89,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 SHORTCODE = config('SHORTCODE')
 PASSKEY = config('PASSKEY')
