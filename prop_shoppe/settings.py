9009add4b8e0b5f113f40db033c0af945f28c40b
Eeury <callmeeury@gmail.com>
2025-07-01 06:56:54 +0300

    Intial commit

--- prop_shoppe/settings.py
+++ prop_shoppe/settings.py
@@ -12,6 +12,7 @@ ALLOWED_HOSTS = ['propshoppe-backend.onrender.com', '*']
 print("ALLOWED_HOSTS:", ALLOWED_HOSTS)
 
 INSTALLED_APPS = [
+    "grappelli",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- prop_shoppe/urls.py
+++ prop_shoppe/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.http import JsonResponse  
 
 urlpatterns = [
+    path("grappelli/", include("grappelli.urls")),
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
     path('api/', include('payments.urls')),
