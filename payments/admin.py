81d954f57466fffdd6d8ab546a38acd5ac2f3201
Eeury <callmeeury@gmail.com>
2025-07-01 18:09:57 +0300

    Initial commit

--- payments/admin.py
+++ payments/admin.py
@@ -1,12 +1,17 @@
-
-from django.contrib import admin
-
-
 from django.contrib import admin
 from .models import PaymentTransaction
 
 @admin.register(PaymentTransaction)
 class PaymentTransactionAdmin(admin.ModelAdmin):
-    list_display = ("id", "phone", "product", "amount", "status", "timestamp", "user", "checkout_request_id", "mpesa_receipt")
+    list_display = (
+        "id",
+        "phone",
+        "amount",
+        "status",
+        "timestamp",
+        "user",
+        "checkout_request_id",
+        "mpesa_receipt"
+    )
     search_fields = ("phone", "checkout_request_id", "mpesa_receipt")
     list_filter = ("status", "timestamp")
