1f8a8b83c48ac90eed4727c6f703cbf859ad09f2
Eeury <callmeeury@gmail.com>
2025-07-07 15:39:26 +0300

    Initial commit

--- products/models.py
+++ products/models.py
@@ -3,19 +3,15 @@ from django.contrib.auth.models import User
 from django.conf import settings
 
 class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
+    name = models.CharField(max_length=100)
     def __str__(self):
         return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    image = models.FileField(upload_to='products/', null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
 
