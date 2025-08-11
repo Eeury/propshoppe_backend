3dcd0d00059eb8f2ca976d408ae02227d7c51a77
Eeury <callmeeury@gmail.com>
2025-08-11 17:23:17 +0300

    Initial commit

--- prop_shoppe/urls.py
+++ prop_shoppe/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+       path('grappelli/', include('grappelli.urls')), 
        path('admin/', admin.site.urls),
        path('api/token-auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
        path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
